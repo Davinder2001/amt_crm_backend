97102d2bf05fb13bf27792ccb668c16ba973acc2
Davinder <162427407+Davinder2001@users.noreply.github.com>
2025-05-15 18:33:23 +0530

    updted code

--- app/Http/Controllers/Api/InvoicesController.php
+++ app/Http/Controllers/Api/InvoicesController.php
@@ -258,7 +258,7 @@ class InvoicesController extends Controller
                 'service_charge_final'      => $finalServiceCharge,
                 'discount_amount'           => $discountAmount ?? 0,
                 'discount_percentage'       => $discountPercentage?? 0,
-                'final_amount'              => $finalAmount,
+                'final_amount'              => $finalAmount ?? $subtotal,
                 'payment_method'            => $data['payment_method'],
                 'issued_by'                 => $issuedById,
                 'company_id'                => $selectedCompany->id,
