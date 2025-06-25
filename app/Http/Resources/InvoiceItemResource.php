bab53916b7470ae7aa2c305921834a064b30dd9a
Davinder <162427407+Davinder2001@users.noreply.github.com>
2025-06-25 19:00:26 +0530

    updated code

--- app/Http/Resources/InvoiceItemResource.php
+++ app/Http/Resources/InvoiceItemResource.php
@@ -22,6 +22,8 @@ class InvoiceItemResource extends JsonResource
             'images'             => $this->images,
             'availability_stock' => $this->availability_stock,
             'catalog'            => (bool) $this->catalog,
+            'units_in_peace'      => $this->units_in_peace,
+            'price_per_unit'      => $this->price_per_unit,
             'online_visibility'  => (bool) $this->online_visibility,
 
             'brand'              => new ItemBrandResource($this->whenLoaded('brand')),
